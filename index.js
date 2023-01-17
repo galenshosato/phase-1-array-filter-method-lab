354ef060a3e207081b89fbac39ce7feee73da441
Galen Sho Sato <galen.sato@gmail.com>
2023-01-16 22:23:20 -0500

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution here
+
+function findMatching (driverNames, name) {
+    return driverNames.filter((el) => el.toLowerCase() === name.toLowerCase())
+}
+
+function fuzzyMatch(driverNames, string) {
+        return driverNames.filter((el) => {
+        if (el.slice(0,2) === string) {
+            return el
+        }
+
+    })
+}
+
+function matchName (driverNames, string) {
+    return driverNames.filter((el) => {
+        if (el['name'] === string) {
+            return el
+        }
+    })
+}
+
+
